59fd64c999340b16fdf579c696b40db9afbabe2e
Sander Steffann <sander@steffann.nl>
2016-09-27 00:20:28 +0200

    Try to stay backwards compatible

--- nat64check/settings.py
+++ nat64check/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'v6score',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
