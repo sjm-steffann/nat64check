0c3811a0a96a4f4ea60a71a4b8cabf81aeac75a0
Sander Steffann <sander@steffann.nl>
2016-10-11 11:28:04 +0200

    Fix dependencies

--- nat64check/settings.py
+++ nat64check/settings.py
@@ -43,6 +43,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'nat64check.urls'
