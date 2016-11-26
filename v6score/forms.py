c80c45c3901b83f31b1445b062ee3558e6677cec
Sander Steffann <sander@steffann.nl>
2016-11-26 14:37:08 +0100

    Fix Chrome input annoyance

--- v6score/forms.py
+++ v6score/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from django.forms.fields import URLField, BooleanField
+from django.forms.fields import BooleanField, URLField
 
 
 class URLForm(forms.Form):
-    url = URLField(widget=forms.URLInput(attrs={'title': 'URL',
-                                                'placeholder': 'http://www.example.com/'}),
+    url = URLField(widget=forms.TextInput(attrs={'title': 'URL',
+                                                 'placeholder': 'http://www.example.com/'}),
                    required=True)
     force_new = BooleanField(required=False)
